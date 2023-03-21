e9ed6eaa8b1de04f701e5a548c7ba789dc681938
ChattiAchrafTn <57968222+ChattiAchrafTn@users.noreply.github.com>
2023-03-21 14:22:36 +0100

    Update webapp.py

--- api/webapp.py
+++ api/webapp.py
@@ -51,4 +51,4 @@ if __name__ == "__main__":
 
     model = YOLO('best.pt')  # force_reload = recache latest code
     
-    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
+    app.run(debug=True, host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
