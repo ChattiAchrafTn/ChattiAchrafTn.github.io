02f70a5cb2563ff67a1bf7c0f5e1462fa1fb4db2
ChattiAchrafTn <57968222+ChattiAchrafTn@users.noreply.github.com>
2023-03-21 17:20:50 +0100

    Update restapi.py

--- api/restapi.py
+++ api/restapi.py
@@ -1,16 +1,17 @@
 """
 Run a rest API exposing the yolov8n_custom object detection model
 """
+from ultralytics import YOLO
 import argparse
 import io
 from PIL import Image
 
 import torch
-from flask import Flask, request
+from flask import Flask, redirect, request
 
 app = Flask(__name__)
 
-DETECTION_URL = "/v1/object-detection/yolov8"
+DETECTION_URL = "/object-detection"
 
 
 @app.route(DETECTION_URL, methods=["POST"])
@@ -22,14 +23,20 @@ def predict():
         image_file = request.files["image"]
         image_bytes = image_file.read()
         img = Image.open(io.BytesIO(image_bytes))
-        results = model(img, size=640) 
-        return results.pandas().xyxy[0].to_json(orient="records")
+        results = model(img)
+        res_plotted = results[0].plot(show_conf =True)
+        for result in results:
+            boxes = result.boxes  # Boxes object for bbox outputs
+            masks = result.masks  # Masks object for segmenation masks outputs
+            probs = result.probs
+        img_savename = f"static/result.png"
+        return redirect(img_savename)
+        #return results.pandas().xyxy[0].to_json(orient="records")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Flask api exposing yolov8 model")
+    parser = argparse.ArgumentParser(description="Flask api exposing yolov5 model")
     parser.add_argument("--port", default=5000, type=int, help="port number")
-    parser.add_argument('--model', default='yolov8n', help='model to run, i.e. --model yolov8n')
     args = parser.parse_args()
 
-    model = torch.hub.load('ultralytics/yolov8', args.model)
+    model = YOLO('best.pt')
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
